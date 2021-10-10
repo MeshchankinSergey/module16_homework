613612cb75394697e0abe25c371d49ba9af7d3f7
Meshchankin Sergey <meshchankin.s@ya.ru>
2021-10-10 18:56:07 +0500

    add task 16.2

--- task_16.2.js
+++ task_16.2.js
@@ -1,1 +1,16 @@
-// {"name":"Anton","age":36,"skills":["Javascript","HTML","CSS"],"salary":80000} 
+const jsonString = `{"name":"Anton","age":36,"skills":["Javascript","HTML","CSS"],"salary":80000}`;
+console.log(jsonString);
+
+const developer = JSON.parse(jsonString);
+
+const result = {
+    name: developer.name,
+    age: developer.age,
+    skills: developer.skills,
+    salary: developer.salary,
+  };
+
+console.log('result', result); // выводит объект js в косоль
+
+const myString = JSON.stringify(developer);
+console.log(myString); // выводит JSON в консоль
