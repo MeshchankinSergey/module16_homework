5a877c77f9e6847948b4ac22fee28923025039b0
Meshchankin Sergey <meshchankin.s@ya.ru>
2021-10-11 19:54:35 +0500

    finel.1 task 16.1

--- task_16.1.js
+++ task_16.1.js
@@ -1,6 +1,6 @@
 const parser = new DOMParser();
 
-const xmlString = `<list><student id="first"><name lang="en"><first>Ivan</first><second>Ivanov</second></name><age>35</age><prof>teacher</prof></student><student id="second"><name lang="ru"><first>Петр</first><second>Петров</second></name><age>58</age><prof>driver</prof></student></list>`;
+const xmlString = `<list><student><name lang="en"><first>Ivan</first><second>Ivanov</second></name><age>35</age><prof>teacher</prof></student><student><name lang="ru"><first>Петр</first><second>Петров</second></name><age>58</age><prof>driver</prof></student></list>`;
 
 const xmlDOM = parser.parseFromString(xmlString, "text/xml");
 
